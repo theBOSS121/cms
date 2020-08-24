cac0633c2e40dca1d2d24843b0e63ffc406b3ca1
Luka Uranic <luka.uranic12@gmail.com>
2020-08-24 20:28:30 +0200

    added typing and 5 btn cool effects

--- js/typing.js
+++ js/typing.js
@@ -1,16 +1,79 @@
 function setUpTyping() {
     for (let i = 0; i < settings.typing.length; i++) {
         let typing = settings.typing[i];
-        let typingHTMLElement = document.querySelector(typing.id);
-        let strings = typing.strings;
-        let index = 0;
-        let displayedString = "";
-        let currentString = strings[index];
-        let currentStringLength = currentString.length;
-
-        displayedString = strings[index].slice(0, currentStringLength / 2);
-
-        typingHTMLElement.innerHTML = `<p>${displayedString}</p>`;
+        if (!document.querySelector(typing.id)) {
+            // console.warn("typing HTMLElement with id=" + typing.id + " does not exist in HTML");
+            continue;
+        }
+        type(
+            typing.strings,
+            document.querySelector(typing.id),
+            typing.typingSpeed,
+            typing.startDelay,
+            typing.endDelay,
+            true,
+            0
+        );
         // console.log(strings);
     }
+}
+
+function type(
+    strings,
+    typingHTMLElement,
+    typingSpeed = 500,
+    startDelay = 500,
+    endDelay = 500,
+    goingUp = true,
+    index = 0,
+    currentStringLength = 0
+) {
+    let displayedString = "";
+    let currentString = strings[index];
+
+    let typingInterval = setInterval(() => {
+        if (goingUp) {
+            currentStringLength++;
+            if (currentStringLength == currentString.length) {
+                goingUp = false;
+
+                clearInterval(typingInterval);
+                setTimeout(() => {
+                    type(
+                        strings,
+                        typingHTMLElement,
+                        typingSpeed,
+                        startDelay,
+                        endDelay,
+                        goingUp,
+                        index,
+                        currentStringLength
+                    );
+                }, endDelay);
+            }
+        } else {
+            currentStringLength--;
+            if (currentStringLength == 0) {
+                goingUp = true;
+                index++;
+                if (index >= strings.length) index = 0;
+                currentString = strings[index];
+                clearInterval(typingInterval);
+                setTimeout(() => {
+                    type(
+                        strings,
+                        typingHTMLElement,
+                        typingSpeed,
+                        startDelay,
+                        endDelay,
+                        goingUp,
+                        index,
+                        currentStringLength
+                    );
+                }, startDelay);
+            }
+        }
+        displayedString = currentString.slice(0, currentStringLength);
+        typingHTMLElement.innerHTML = `<p>${displayedString}</p>`;
+    }, typingSpeed);
 }
